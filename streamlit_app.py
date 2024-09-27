f1d89bed095b401fe5d3f4d68e42157f21435fd0
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2024-09-27 11:51:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with col5:
     Select_type = st.selectbox("ประเภท", List_type, key='List_type')
 
     List_place = ['ทั้งหมด']
-    for i in df.สถานที่.unique(): List_place.append(i)
+    for i in df.place.unique(): List_place.append(i)
     Select_place = st.selectbox("สถานที่", List_place, key='List_place')
     
 with col6:
@@ -74,7 +74,7 @@ else:
     if Select_type != 'ทั้งหมด': data = data[data.ประเภท==Select_type]
     if Select_name != 'ทั้งหมด': data = data[data.รายการ==Select_name]
     if Select_status != 'ทั้งหมด': data = data[data.สภาพครุภัณฑ์==Select_status]
-    if Select_place != 'ทั้งหมด': data = data[data.สถานที่==Select_place]  
+    if Select_place != 'ทั้งหมด': data = data[data.place==Select_place]  
     data = data.reset_index(drop=True)
     data.index = np.arange(1, len(data) + 1)
     data.index.name = 'ลำดับ'
