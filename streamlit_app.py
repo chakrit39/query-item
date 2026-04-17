3eeac2e9ad786eabd9b5c91994fc29032f5e9646
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2026-04-17 13:29:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def get_full_data():
         "https://www.googleapis.com/auth/cloud-platform"
     ]
     credentials = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
-    client = bigquery.Client(credentials=credentials, project=info["project_id"])
+    client = bigquery.Client(credentials=credentials, project="dol-workspace")
     
     query = "SELECT * FROM `dol-workspace.Dashboard_Work69.v_master_report`"
     return client.query(query).to_dataframe()
