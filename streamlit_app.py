090a548e1ce4cc3680711e8fddd2dd752592291f
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2026-04-17 14:53:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,7 +191,7 @@ with left_col:
 
 # --- [ฝั่งขวา: เฉพาะวันนี้] ---
 with right_col:
-    r1, r2, r3 = st.columns([0.3, 0.3, 0.4])
+    r1, r2, r3 = st.columns([0.4, 0.2, 0.4])
     r1.header(f"📅 ผลงานตามวันที่")
     selected_date = r2.date_input("", today, key="date_selector")
     
