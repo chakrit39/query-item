06d09b260bc5f7bb3f4a4a5be36e36e46e1ce2eb
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2026-04-19 02:23:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,6 +157,8 @@ fig.update_layout(
 # 5. แสดงกราฟ
 st.plotly_chart(fig, use_container_width=True)
 
+# 1. สร้างคอลัมน์ระบุวันในสัปดาห์ (0=จันทร์, 5=เสาร์, 6=อาทิตย์)
+trend_data_30['day_of_week'] = trend_data_30['DATE_SUBMIT'].dt.dayofweek
 # 2. กรองข้อมูลเฉพาะ: ไม่ใช่เสาร์(5), ไม่ใช่อาทิตย์(6) และ ยอดงานต้องมากกว่า 0
 filtered_for_avg = trend_data_30[
     (trend_data_30['day_of_week'] < 5) & 
