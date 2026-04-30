d3e2007d5c63f91ea8b51450d1df3bc2f4bfd274
chakrit39 <126540185+chakrit39@users.noreply.github.com>
2026-05-01 01:07:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -228,13 +228,13 @@ def summary_with_metrics_v2(input_df, group_col, df_tor, target_date, show_total
 def display_styled_dataframe_v2(df_display, title):
     st.subheader(title)
     dynamic_height = 35 * (len(df_display) + 1)
-    df.style\
+    df_display.style\
         .set_properties(subset=["NAME"], **{'border-right': '3px solid black', 'border-left': '3px solid black'})\
         .set_properties(subset=["มอบหมาย"], **{'border-left': '3px solid black'})\
         .set_properties(subset=["ดำเนินการแล้ว"], **{'border-right': '3px solid black'})\
         .set_properties(subset=["เป้าสะสม (TOR)"], **{'border-left': '3px solid black'})\
         .set_properties(subset=["ผลงาน (TOR)"], **{'border-right': '3px solid black'})
-    df_display = style_groups(df)
+
     st.dataframe(
         df_display,
         column_config={
